90bda405816fb173f5f2c6d118f61451004cec03
Avinash <avkumar@sigmasolve.com>
2025-03-05 10:45:13 +0530

    update the cors allow to all service

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const nftRoutes = require('./routes/nft');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(morgan("dev"));
 
 mongoose
